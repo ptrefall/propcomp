c2153147b1c5ba9f020b015b8fbed34471574389
PTrefall@gmail.com <PTrefall@gmail.com@1ed68ad0-91c7-136a-175d-75a241914234>
2011-10-03 21:04:46 +0000

    Moved some "examples" from test folder to new examples folder. Also added a tools folder.
    In root cmake file, there's now the option of enabling inclusion of tests, examples and tools with the project generation (appears as checkboxes in the CMake gui tool).
    
    Tests are now reserved for focused tests of specific features, while examples are a bit wider and there to display more real-life usage examples, even working games.
    
    Tools folder will hold div tools, most notably the Entity Editor.

---
+++ examples/ElCar/Components/Throttle.cpp
@@ -0,0 +1,68 @@
+/*Factotum EDK
+Copyright (c) 2009 Pål Trefall and Kenneth Gangstø
+
+This software is provided 'as-is', without any express or implied
+warranty.  In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented; you must not
+   claim that you wrote the original software. If you use this software
+   in a product, an acknowledgment in the product documentation would be
+   appreciated but is not required.
+2. Altered source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+3. This notice may not be removed or altered from any source distribution.
+
+Note: Some of the libraries Factotum EDK may link to may have additional
+requirements or restrictions.
+*/
+
+#include "Throttle.h"
+
+#include <iostream>
+
+using namespace Components;
+using namespace Factotum;
+
+Throttle::Throttle(Entity &owner, const T_String &name)
+: Component(owner, name), throttleEventId("THROTTLE"), spendEffectEventId("SPEND_EFFECT"), accelerateWheelsEventId("ACCELERATE_WHEELS")
+{
+	type_property = owner.addProperty<T_String>("Type", "");
+	velocity_property = owner.addProperty<F32>("Velocity", 0.0f);
+	maxVelocity_property = owner.addProperty<F32>("MaxVelocity", 100.0f);
+	engineForce_property = owner.addProperty<F32>("EngineForce", 1.0f);
+	engineEffect_property = owner.addProperty<F32>("EngineEffect", 1.0f);
+	mass_property = owner.addProperty<F32>("Mass", 1.0f);
+
+	owner.registerToEvent0(throttleEventId).connect(this, &Throttle::onThrottleEvent);
+}
+
+Throttle::~Throttle()
+{
+}
+
+void Throttle::onThrottleEvent()
+{
+	std::cout << "Player presses the gas pedal of his " << type_property.get().c_str() << std::endl;
+
+	engineForce_property = engineEffect_property.get();
+
+	//We spend some energy forcing the wheels to turn
+#if USE_TEMPLATE_EVENT_HANDLER
+	owner.sendEvent1<F32>(spendEffectEventId, engineEffect_property.get());
+#elif USE_ANY_EVENT_HANDLER
+	owner.sendEvent(spendEffectEventId, T_Any(engineEffect_property.get()));
+#endif
+	
+		
+	//Throttling forces wheels to rotate by an acceleration
+#if USE_TEMPLATE_EVENT_HANDLER
+	owner.sendEvent1<F32>(accelerateWheelsEventId, engineForce_property.get());
+#elif USE_ANY_EVENT_HANDLER
+	owner.sendEvent(accelerateWheelsEventId, T_Any(engineForce_property.get()));
+#endif
+}
---
+++ examples/ElCar/Components/WheelMount.cpp
@@ -0,0 +1,95 @@
+/*Factotum EDK
+Copyright (c) 2009 Pål Trefall and Kenneth Gangstø
+
+This software is provided 'as-is', without any express or implied
+warranty.  In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented; you must not
+   claim that you wrote the original software. If you use this software
+   in a product, an acknowledgment in the product documentation would be
+   appreciated but is not required.
+2. Altered source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+3. This notice may not be removed or altered from any source distribution.
+
+Note: Some of the libraries Factotum EDK may link to may have additional
+requirements or restrictions.
+*/
+
+#include "WheelMount.h"
+#include "../../Common/EntityManager.h"
+
+#include <iostream>
+
+using namespace Components;
+using namespace Factotum;
+
+WheelMount::WheelMount(Entity &owner, const T_String &name)
+: Component(owner, name), accelerateWheelsEventId("ACCELERATE_WHEELS"), syncVelocityEventId("SYNC_VELOCITY"), forceAngularAccelerationEventId("FORCE_ANGULAR_ACCELERATION")
+{
+	type_property = owner.addProperty<T_String>("Type", "");
+    wheels_property_list = owner.addPropertyList<Entity*>("Wheels");
+	activeWheelCount_property = owner.addProperty<U32>("ActiveWheelCount", 2); //Two wheels can accelerate by throttling by default
+	velocity_property = owner.addProperty<F32>("Velocity", 0.0f);
+
+#if USE_TEMPLATE_EVENT_HANDLER
+	owner.registerToEvent1<F32>(accelerateWheelsEventId).connect(this, &WheelMount::onAccelerateWheelsEvent);
+#elif USE_ANY_EVENT_HANDLER
+	owner.registerToEvent1(accelerateWheelsEventId).connect(this, &WheelMount::onAccelerateWheelsEvent);
+#endif
+}
+
+WheelMount::~WheelMount()
+{
+}
+
+void WheelMount::update(const F32 &/*deltaTime*/)
+{
+	F32 avgVelocity = 0.0f;
+	for(U32 i = 0; i < wheels_property_list.size(); i++)
+	{
+		if(wheels_property_list.get()[i]->hasProperty("AngularVelocity"))
+			avgVelocity += wheels_property_list.get()[i]->getProperty<F32>("AngularVelocity").get();
+	}
+	avgVelocity /= wheels_property_list.size();
+	velocity_property = avgVelocity;
+	std::cout << type_property.get().c_str() << " velocity changed to " << avgVelocity << std::endl;
+
+	//Now force all wheels to turn at same speed as the car
+	for(U32 i = 0; i < wheels_property_list.size(); i++)
+	{
+		for(U32 i = 0; i < wheels_property_list.size(); i++)
+		{
+#if USE_TEMPLATE_EVENT_HANDLER
+			wheels_property_list.get()[i]->sendEvent1<F32>(syncVelocityEventId, avgVelocity);
+#elif USE_ANY_EVENT_HANDLER
+			wheels_property_list.get()[i]->sendEvent(syncVelocityEventId, T_Any(avgVelocity));
+#endif
+			
+		}
+	}
+}
+
+#if USE_TEMPLATE_EVENT_HANDLER
+void WheelMount::onAccelerateWheelsEvent(const F32 &force)
+{
+#elif USE_ANY_EVENT_HANDLER
+void WheelMount::onAccelerateWheelsEvent(T_Any force_any)
+{
+	//const F32 &force = force_any.cast<F32>();
+#endif
+	//Force wheels to spin
+	for(U32 i = 0; i < wheels_property_list.size() && i < activeWheelCount_property.get(); i++)
+	{
+#if USE_TEMPLATE_EVENT_HANDLER
+		wheels_property_list.get()[i]->sendEvent1<F32>(forceAngularAccelerationEventId, force);
+#elif USE_ANY_EVENT_HANDLER
+		wheels_property_list.get()[i]->sendEvent(forceAngularAccelerationEventId, force_any);
+#endif
+	}
+}
