71bac4dbb7b56bf7ed403ee832b05de663560a5c
sphair@gmail.com <sphair@gmail.com@1ed68ad0-91c7-136a-175d-75a241914234>
2011-11-24 12:16:16 +0000

    - new preprocessor that does src/include/examples/tests

--- trunk/depends/ClanLib/preprocessor.sh
+++ trunk/depends/ClanLib/preprocessor.sh
@@ -1,24 +1,26 @@
-mv ../../src/types_config.h ../../src/types_config.h.old
-sed -i 's/T_String/CL_String/g' ../../src/*.h ../../src/*.cpp
-sed -i 's/D32/double/g' ../../src/*.h ../../src/*.cpp
-sed -i 's/F32/float/g' ../../src/*.h ../../src/*.cpp
-sed -i 's/S32/int/g' ../../src/*.h ../../src/*.cpp
-sed -i 's/U32/unsigned int/g' ../../src/*.h ../../src/*.cpp
-sed -i 's/T_PropertyTypeId/unsigned int/g' ../../src/*.h ../../src/*.cpp
-sed -i 's/T_EntityId/unsigned long/g' ../../src/*.h ../../src/*.cpp
-sed -i 's/T_Exception/CL_Exception/g' ../../src/*.h ../../src/*.cpp
-sed -i 's/HAS_SIGNALSLOTS_INHERITANCE_TYPE//g' ../../src/*.h ../../src/*.cpp
-sed -i 's/NULL_PTR/nullptr/g' ../../src/*.h ../../src/*.cpp
-sed -i 's/typename T_Signal_v0<>::Type/CL_Signal_v0/g' ../../src/*.h ../../src/*.cpp
-sed -i 's/T_Signal_v0<>::Type/CL_Signal_v0/g' ../../src/*.h ../../src/*.cpp
-sed -i 's/typename T_Signal_v\(.*\)::Type/CL_Signal_v\1/g' ../../src/*.h ../../src/*.cpp
-sed -i 's/T_Signal_v\(.*\)::Type/CL_Signal_v\1/g' ../../src/*.h ../../src/*.cpp
-sed -i 's/typename T_SharedPtr\(.*\)::Type/CL_SharedPtr\1/g' ../../src/*.h ../../src/*.cpp
-sed -i 's/T_SharedPtr\(.*\)::Type/CL_SharedPtr\1/g' ../../src/*.h ../../src/*.cpp
-sed -i 's/typename T_Vector\(.*\)::Type/std::vector\1/g' ../../src/*.h ../../src/*.cpp
-sed -i 's/T_Vector\(.*\)::Type/std::vector\1/g' ../../src/*.h ../../src/*.cpp
-sed -i 's/typename T_Map\(.*\)::Type/std::unordered_map\1/g' ../../src/*.h ../../src/*.cpp
-sed -i 's/T_Map\(.*\)::Type/std::unordered_map\1/g' ../../src/*.h ../../src/*.cpp
-sed -i 's/typename T_Pair\(.*\)::Type/std::pair\1/g' ../../src/*.h ../../src/*.cpp
-sed -i 's/T_Pair\(.*\)::Type/std::pair\1/g' ../../src/*.h ../../src/*.cpp
-mv ../../src/types_config.h.old ../../src/types_config.h
+mv ../../include/Totem/types_config.h ../../include/Totem/types_config.h.old
+
+find ../../src ../../include ../../examples ../../tests -type f \( -name "*.cpp" -o -name "*.h" \) -exec sed -i 's/T_String/CL_String/g' {} \;
+find ../../src ../../include ../../examples ../../tests -type f \( -name "*.cpp" -o -name "*.h" \) -exec sed -i 's/D32/double/g' {} \;
+find ../../src ../../include ../../examples ../../tests -type f \( -name "*.cpp" -o -name "*.h" \) -exec sed -i 's/F32/float/g' {} \;
+find ../../src ../../include ../../examples ../../tests -type f \( -name "*.cpp" -o -name "*.h" \) -exec sed -i 's/S32/int/g' {} \;
+find ../../src ../../include ../../examples ../../tests -type f \( -name "*.cpp" -o -name "*.h" \) -exec sed -i 's/U32/unsigned int/g' {} \;
+find ../../src ../../include ../../examples ../../tests -type f \( -name "*.cpp" -o -name "*.h" \) -exec sed -i 's/T_PropertyTypeId/unsigned int/g' {} \;
+find ../../src ../../include ../../examples ../../tests -type f \( -name "*.cpp" -o -name "*.h" \) -exec sed -i 's/T_EntityId/unsigned long/g' {} \;
+find ../../src ../../include ../../examples ../../tests -type f \( -name "*.cpp" -o -name "*.h" \) -exec sed -i 's/T_Exception/CL_Exception/g' {} \;
+find ../../src ../../include ../../examples ../../tests -type f \( -name "*.cpp" -o -name "*.h" \) -exec sed -i 's/HAS_SIGNALSLOTS_INHERITANCE_TYPE//g' {} \;
+find ../../src ../../include ../../examples ../../tests -type f \( -name "*.cpp" -o -name "*.h" \) -exec sed -i 's/NULL_PTR/nullptr/g' {} \;
+find ../../src ../../include ../../examples ../../tests -type f \( -name "*.cpp" -o -name "*.h" \) -exec sed -i 's/typename T_Signal_v0<>::Type/CL_Signal_v0/g' {} \;
+find ../../src ../../include ../../examples ../../tests -type f \( -name "*.cpp" -o -name "*.h" \) -exec sed -i 's/T_Signal_v0<>::Type/CL_Signal_v0/g' {} \;
+find ../../src ../../include ../../examples ../../tests -type f \( -name "*.cpp" -o -name "*.h" \) -exec sed -i 's/typename T_Signal_v\(.*\)::Type/CL_Signal_v\1/g' {} \;
+find ../../src ../../include ../../examples ../../tests -type f \( -name "*.cpp" -o -name "*.h" \) -exec sed -i 's/T_Signal_v\(.*\)::Type/CL_Signal_v\1/g' {} \;
+find ../../src ../../include ../../examples ../../tests -type f \( -name "*.cpp" -o -name "*.h" \) -exec sed -i 's/typename T_SharedPtr\(.*\)::Type/CL_SharedPtr\1/g' {} \;
+find ../../src ../../include ../../examples ../../tests -type f \( -name "*.cpp" -o -name "*.h" \) -exec sed -i 's/T_SharedPtr\(.*\)::Type/CL_SharedPtr\1/g' {} \;
+find ../../src ../../include ../../examples ../../tests -type f \( -name "*.cpp" -o -name "*.h" \) -exec sed -i 's/typename T_Vector\(.*\)::Type/std::vector\1/g' {} \;
+find ../../src ../../include ../../examples ../../tests -type f \( -name "*.cpp" -o -name "*.h" \) -exec sed -i 's/T_Vector\(.*\)::Type/std::vector\1/g' {} \;
+find ../../src ../../include ../../examples ../../tests -type f \( -name "*.cpp" -o -name "*.h" \) -exec sed -i 's/typename T_Map\(.*\)::Type/std::unordered_map\1/g' {} \;
+find ../../src ../../include ../../examples ../../tests -type f \( -name "*.cpp" -o -name "*.h" \) -exec sed -i 's/T_Map\(.*\)::Type/std::unordered_map\1/g' {} \;
+find ../../src ../../include ../../examples ../../tests -type f \( -name "*.cpp" -o -name "*.h" \) -exec sed -i 's/typename T_Pair\(.*\)::Type/std::pair\1/g' {} \;
+find ../../src ../../include ../../examples ../../tests -type f \( -name "*.cpp" -o -name "*.h" \) -exec sed -i 's/T_Pair\(.*\)::Type/std::pair\1/g' {} \;
+
+mv ../../include/Totem/types_config.h.old ../../include/Totem/types_config.h
