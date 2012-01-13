ef5e68f56514d4b61782564ec50191dde68a66d2
PTrefall@gmail.com <PTrefall@gmail.com@1ed68ad0-91c7-136a-175d-75a241914234>
2012-01-13 13:57:29 +0000

    Now works with custom parameter.

--- syntax_experiments/TestNewCpp1/main.cpp
+++ syntax_experiments/TestNewCpp1/main.cpp
@@ -14,13 +14,32 @@ public:
 	Component(const std::string &type);
 	virtual ~Component();
 
+	template<typename ComponentType>
+	static unsigned int getTypeId()
+	{
+		static unsigned int id(newPropertyTypeId());
+		return id;
+	}
+
 protected:
 	std::string type;
+	unsigned int typeId;
+	static unsigned int newPropertyTypeId()
+	{
+		static unsigned int nextId(0);
+		return nextId++;
+	}
 
 	template<class ComponentType>
 	static std::shared_ptr<ComponentType> InternalCreate()
 	{
 		return std::make_shared<ComponentType>();
+	}
+
+	template<class ComponentType, class CustomParam0>
+	static std::shared_ptr<ComponentType> InternalCreate(CustomParam0 param0)
+	{
+		return std::make_shared<ComponentType>(param0);
 	}
 };
 
@@ -46,10 +65,17 @@ class ComponentFactory
 public:
 	ComponentFactory() {}
 	
-	template<class T>
-	auto create() -> decltype(T::Create())
+	template<class ComponenType>
+	auto create() -> decltype(ComponenType::Create())
 	{
-		auto component = T::Create();
+		auto component = ComponenType::Create();
+		return component;
+	}
+
+	template<class ComponenType, class CustomParam0>
+	auto create() -> decltype(ComponenType::Create())
+	{
+		auto component = ComponenType::Create();
 		return component;
 	}
 };
@@ -67,14 +93,34 @@ class ComponentHandler
 public:
 	ComponentHandler(ComponentFactory &factory) : factory(factory) {}
 	
-	template<class T>
-	auto addComponent() -> decltype(T::Create())
+	/*auto addComponent(const std::string &type) -> std::shared_ptr<Component>
 	{
-		auto component = T::Create();
+		auto compFuncIt = components.find(type);
+		if(compFuncIt == components.end())
+			throw std::runtime_error("Component type not found!");
+		const unsigned int &typeId = compFuncIt->second;
+		if(typeId == Component::getTypeId<TestComp>())
+			return TestComp::Create();
+
+		throw std::runtime_error("Component type not found!");
+	}*/
+
+	template<class ComponenType>
+	auto addComponent() -> decltype(ComponentType::Create())
+	{
+		auto component = ComponentType::Create();
+		return component;
+	}
+
+	template<class ComponentType, class CustomParam0>
+	std::shared_ptr<ComponentType> addComponent1(CustomParam0 param0)
+	{
+		auto component = ComponentType::Create<CustomParam0>(param0);
 		return component;
 	}
 private:
 	ComponentFactory &factory;
+	//std::unordered_map<std::string, unsigned int> components;
 };
 //
 /////////////////////////////////////////////////////////
@@ -98,11 +144,15 @@ public:
 class TestComp : public Component
 {
 public:
-	TestComp() : Component(Type()) {}
+	TestComp(TestSystem &sys) : Component(Type()), sys(sys) { typeId = Component::getTypeId<TestComp>(); }
 	static std::string Type() { return "Test"; }
-	static auto Create() -> decltype(Component::InternalCreate<TestComp>()) { return Component::InternalCreate<TestComp>(); }
+	template<class CustomParam0>
+	static std::shared_ptr<TestComp> Create(CustomParam0 param0) { return std::make_shared<TestComp>(param0); }
 
-	void test() { std::cout << "Testing!" << std::endl; }
+	void test() { sys.test(); }
+
+private:
+	TestSystem &sys;
 };
 //
 /////////////////////////////////////////////////////////
@@ -116,7 +166,8 @@ void main()
 {
 	ComponentFactory factory;
 	ComponentHandler entity(factory);
-	auto testComp = entity.addComponent<TestComp>();
+	TestSystem sys;
+	auto testComp = entity.addComponent1<TestComp, TestSystem&>(sys);
 	testComp->test();
 	system("pause");
 }
