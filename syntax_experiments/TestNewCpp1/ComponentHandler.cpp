c12a554a2ccc57fb91d8c061f9d490b69eceaeaa
PTrefall@gmail.com <PTrefall@gmail.com@1ed68ad0-91c7-136a-175d-75a241914234>
2012-01-14 06:58:47 +0000

    Serialization support for instantiating components, but the approach can't be internal to Totem with the current implementation, which would be optimal...

--- syntax_experiments/TestNewCpp1/ComponentHandler.cpp
+++ syntax_experiments/TestNewCpp1/ComponentHandler.cpp
@@ -1,2 +1,16 @@
 #include "ComponentHandler.h"
 #include "Component.h"
+
+void ComponentHandler::registerTypeId(const std::string &type, unsigned int typeId)
+{
+	component_type_ids[type] = typeId;
+}
+
+unsigned int ComponentHandler::getTypeId(const std::string &type)
+{
+	auto it = component_type_ids.find(type);
+	if(it == component_type_ids.end())
+		 return 0;
+	else
+		return it->second;
+}
