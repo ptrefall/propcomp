90e6195d56fdb8501f5d9321f1a59355969d56c1
PTrefall@gmail.com <PTrefall@gmail.com@1ed68ad0-91c7-136a-175d-75a241914234>
2012-01-16 22:38:32 +0000

    Added test for valueChanged and support for multiple components of same type. As expected, if a component holding a shared property is added multiple times, then valueChanged will also hit each of those component instances if the component type has subscribed to the valueChanged signal.

--- syntax_experiments/TestNewCpp1/Component.h
+++ syntax_experiments/TestNewCpp1/Component.h
@@ -4,12 +4,13 @@
 
 #include <string>
 #include <memory>
+#include "../../depends/Standard/sigslot.h"
 #include "PropertyContainer.h"
 
 class Component;
 typedef std::shared_ptr<Component> ComponentPtr;
 
-class Component : public PropertyContainer<>
+class Component : public PropertyContainer<>, public sigslot::has_slots<>
 {
 public:
 	Component(const std::string &type) : PropertyContainer<>(), type(type) {}
