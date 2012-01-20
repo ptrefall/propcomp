ed65fd126b3b6bca6da5db60cd2800e53ccd8a4e
PTrefall@gmail.com <PTrefall@gmail.com@1ed68ad0-91c7-136a-175d-75a241914234>
2012-01-20 07:05:46 +0000

    Started writing out the ComponentContainer. It's finished but for implementing all 6 versions of addComponent. For some reason, my linker (vs2010) starts crashing when I use same name for the function and changing number of template arguments and parameters to function... adding a number convention at end of function name fixes the issue, but isn't exactly optimal... this must be a bug in vs2010.

--- syntax_experiments/TestNewCpp1/ComponentContainer.h
+++ syntax_experiments/TestNewCpp1/ComponentContainer.h
@@ -24,6 +24,12 @@ public:
 	{
 		return std::make_shared<ComponentType>(name, entity, param0);
 	}
+
+	template<class EntityType, class ComponentType, class CustomParam0, class CustomParam1>
+	static std::shared_ptr<ComponentType> createComponent(const std::string &name, EntityType *entity, CustomParam0 param0, CustomParam1 param1, unsigned int optionalParam)
+	{
+		return std::make_shared<ComponentType>(name, entity, param0, param1);
+	}
 };
 
 template<class EntityType, class ComponentFactoryType = DefaultComponentFactory>
@@ -33,7 +39,7 @@ public:
 	ComponentContainer(EntityType *thisAsEntity) : thisAsEntity(thisAsEntity) {}
 
 	template<class ComponentType>
-	std::shared_ptr<ComponentType> addComponent(const std::string &name = std::string(), unsigned int optionalParam = 0)
+	std::shared_ptr<ComponentType> addComponent0(const std::string &name = std::string(), unsigned int optionalParam = 0)
 	{
 		auto component = ComponentFactoryType::createComponent<EntityType, ComponentType>(name, thisAsEntity, optionalParam);
 		components.push_back(component);
@@ -41,14 +47,70 @@ public:
 	}
 
 	template<class ComponentType, class CustomParam0>
-	std::shared_ptr<ComponentType> addComponent(CustomParam0 param0, const std::string &name = std::string(), unsigned int optionalParam = 0)
+	std::shared_ptr<ComponentType> addComponent1(CustomParam0 param0, const std::string &name = std::string(), unsigned int optionalParam = 0)
 	{
 		auto component = ComponentFactoryType::createComponent<EntityType, ComponentType, CustomParam0>(name, thisAsEntity, param0, optionalParam);
 		components.push_back(component);
 		return component;
 	}
 
+	template<class ComponentType, class CustomParam0, class CustomParam1>
+	std::shared_ptr<ComponentType> addComponent2(CustomParam0 param0, CustomParam1 param1, const std::string &name = std::string(), unsigned int optionalParam = 0)
+	{
+		auto component = ComponentFactoryType::createComponent<EntityType, ComponentType, CustomParam0, CustomParam1>(name, thisAsEntity, param0, param1, optionalParam);
+		components.push_back(component);
+		return component;
+	}
 
+	//////////////////////////////////////////////////////////////////////////////
+
+	template<class ComponentType>
+	bool hasComponent(const std::string &name = std::string())
+	{
+		for(unsigned int i = 0; i < components.size(); i++)
+		{
+			if(components[i]->getType() == ComponentType::Type())
+			{
+				if(!name.empty())
+				{
+					if(components[i]->getName() == name)
+						return true;
+				}
+				else
+				{
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	template<class ComponentType>
+	std::shared_ptr<ComponentType> getComponent(const std::string &name = std::string())
+	{
+		for(unsigned int i = 0; i < components.size(); i++)
+		{
+			if(components[i]->getType() == ComponentType::Type())
+			{
+				if(!name.empty())
+				{
+					if(components[i]->getName() == name)
+						return static_pointer_cast<ComponentType>(components[i]);
+				}
+				else
+				{
+					return static_pointer_cast<ComponentType>(components[i]);
+				}
+			}
+		}
+		throw std::runtime_error(("Couldn't find component " + ComponentType::Type()).c_str());
+	}
+
+	void updateComponents(const float &deltaTime)
+	{
+		for(unsigned int i = 0; i < components.size(); i++)
+			components[i]->update(deltaTime);
+	}
 
 protected:
 	EntityType *thisAsEntity;
