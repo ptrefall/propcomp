007ca384ff3a9ff68b0ce0edc7c8ec15e1293fd5
sphair@gmail.com <sphair@gmail.com@1ed68ad0-91c7-136a-175d-75a241914234>
2012-05-08 19:34:12 +0000

    - Added horizontal movement component - Cleaned up app abit

--- examples/ClanLib/RedBall/redballapp.h
+++ examples/ClanLib/RedBall/redballapp.h
@@ -11,8 +11,12 @@ public:
 private:
 	void on_window_close();
 	void on_input_up(const CL_InputEvent &key);
-
+	float calculate_delta_time();
+	
 	bool quit;
+	unsigned int last_time;
 
 	std::vector<Entity *> entities;
+
+	CL_SlotContainer slots;
 };
