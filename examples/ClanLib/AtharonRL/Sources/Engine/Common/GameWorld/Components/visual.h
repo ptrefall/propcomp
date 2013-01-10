2909196b213dc813523739787ab1ada0ada41e36
ptrefall@gmail.com <ptrefall@gmail.com@1ed68ad0-91c7-136a-175d-75a241914234>
2013-01-10 14:32:43 +0000

    Changed name from looks to visual

---
+++ examples/ClanLib/AtharonRL/Sources/Engine/Common/GameWorld/Components/visual.h
@@ -0,0 +1,27 @@
+
+#pragma once
+
+#include "Engine/Common/GameWorld/gameobject.h"
+
+enum GameObjectVisual
+{
+	GOV_CHARACTER = 0,
+	GOV_ORC,
+	GOV_TROLL,
+	GOV_ITEM,
+	GOV_SCROLL,
+	GOV_POTION,
+	GOV_CORPSE
+};
+
+class Visual : public Totem::Component<Visual>
+{
+public:
+	Visual(GameObject &owner, const std::string &name = std::string());
+	virtual ~Visual();
+
+	static std::string getType() { return "Visual"; }
+
+protected:
+	Totem::Property<int> looks_property;
+};
