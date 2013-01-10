a4715f14751dcb0cb826547be49ffba6fd22428e
PTrefall@gmail.com <PTrefall@gmail.com@1ed68ad0-91c7-136a-175d-75a241914234>
2013-01-10 21:18:46 +0000

    fixed

--- trunk/examples/ClanLib/AtharonRL/Sources/Server/GameWorld/Components/server_visual.h
+++ trunk/examples/ClanLib/AtharonRL/Sources/Server/GameWorld/Components/server_visual.h
@@ -4,14 +4,9 @@
 #include "Engine/Common/GameWorld/Components/visual.h"
 #include "Server/GameWorld/replicated_component.h"
 
-class ServerVisual : public Totem::Component<ServerVisual>, public ReplicatedComponent
+class ServerVisual : public Visual, public ReplicatedComponent
 {
 public:
 	ServerVisual(GameObject *owner, const std::string &name = std::string());
 	virtual ~ServerVisual();
-
-	static std::string getType() { return "Visual"; }
-
-protected:
-	Totem::Property<int> visual_property;
 };
