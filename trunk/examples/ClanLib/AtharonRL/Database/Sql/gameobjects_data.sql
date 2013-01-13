77fbbd6227a741d50035b09ebc63d034b536dcb4
PTrefall@gmail.com <PTrefall@gmail.com@1ed68ad0-91c7-136a-175d-75a241914234>
2013-01-13 06:35:25 +0000

    Changed IsActive to LoadByDefault flag in the gameobject database, and fixed all related functions in the server code. Should probably go through the entire flow that interacted with InActive before and make sure that everything is set up correctly now, but at least this fixed the issue of the server loading your character two times when logging back in after a server crash.

--- trunk/examples/ClanLib/AtharonRL/Database/Sql/gameobjects_data.sql
+++ trunk/examples/ClanLib/AtharonRL/Database/Sql/gameobjects_data.sql
@@ -1,20 +1,20 @@
 BEGIN TRANSACTION;
 
-INSERT INTO GameObjects (GameObjectId, GameObjectContainerId, IsActive, Description) VALUES (2, 1, 0, 'Tyrell');
-INSERT INTO GameObjects (GameObjectId, GameObjectContainerId, IsActive, Description) VALUES (3, 1, 0, 'Nightwind');
-INSERT INTO GameObjects (GameObjectId, GameObjectContainerId, IsActive, Description) VALUES (4, 1, 0, 'Sphair');
-INSERT INTO GameObjects (GameObjectId, GameObjectContainerId, IsActive, Description) VALUES (5, 1, 0, 'Judas');
-INSERT INTO GameObjects (GameObjectId, GameObjectContainerId, IsActive, Description) VALUES (6, 1, 0, 'Woody');
+INSERT INTO GameObjects (GameObjectId, GameObjectContainerId, LoadByDefault, Description) VALUES (2, 1, 0, 'Tyrell');
+INSERT INTO GameObjects (GameObjectId, GameObjectContainerId, LoadByDefault, Description) VALUES (3, 1, 0, 'Nightwind');
+INSERT INTO GameObjects (GameObjectId, GameObjectContainerId, LoadByDefault, Description) VALUES (4, 1, 0, 'Sphair');
+INSERT INTO GameObjects (GameObjectId, GameObjectContainerId, LoadByDefault, Description) VALUES (5, 1, 0, 'Judas');
+INSERT INTO GameObjects (GameObjectId, GameObjectContainerId, LoadByDefault, Description) VALUES (6, 1, 0, 'Woody');
 
 -- SPIDER
-INSERT INTO GameObjects (GameObjectId, GameObjectContainerId, IsActive, Description) VALUES (11, 1, 1, 'Orc');
+INSERT INTO GameObjects (GameObjectId, GameObjectContainerId, LoadByDefault, Description) VALUES (11, 1, 1, 'Orc');
 INSERT INTO GameObjectComponents (GameObjectId, Type) VALUES (11, "Visual");
 INSERT INTO GameObjectComponents (GameObjectId, Type) VALUES (11, "Transform");
 INSERT INTO GameObjectProperties (GameObjectId, Type, Name, Value) VALUES (11, 2, 	"Visual", "1");
 INSERT INTO GameObjectProperties (GameObjectId, Type, Name, Value) VALUES (11, 101, "Position", "10 10");
 
 -- TRIGGER
---INSERT INTO GameObjects (GameObjectId, GameObjectContainerId, IsActive, Description) VALUES (12, 1, 1, 'Trigger');
+--INSERT INTO GameObjects (GameObjectId, GameObjectContainerId, LoadByDefault, Description) VALUES (12, 1, 1, 'Trigger');
 --INSERT INTO GameObjectComponents (GameObjectId, Type) VALUES (12, "Collider");
 --INSERT INTO GameObjectProperties (GameObjectId, Type, Name, Value) VALUES (12, 101, "Position", "15 2");
 --INSERT INTO GameObjectComponents (GameObjectId, Type) VALUES (12, "TriggerDisplayText");
@@ -22,7 +22,7 @@ INSERT INTO GameObjectProperties (GameObjectId, Type, Name, Value) VALUES (11, 1
 --INSERT INTO GameObjectProperties (GameObjectId, Type, Name, Value) VALUES (12, 100, "DisplayTextOnExit", "You left the trigger");
 
 -- PLAYER ECO
-INSERT INTO GameObjects (GameObjectId, GameObjectContainerId, IsActive, Description) VALUES (1, 1, 0, 'Eco');
+INSERT INTO GameObjects (GameObjectId, GameObjectContainerId, LoadByDefault, Description) VALUES (1, 1, 0, 'Eco');
 --INSERT INTO GameObjectComponents (GameObjectId, Type) VALUES (1, "Monster");
 --INSERT INTO GameObjectComponents (GameObjectId, Type) VALUES (1, "ClickMoveController");
 --INSERT INTO GameObjectComponents (GameObjectId, Type) VALUES (1, "NumpadMoveController");
--- trunk/examples/ClanLib/AtharonRL/Sources/Server/Database/database_gameobject_containers.h
+++ trunk/examples/ClanLib/AtharonRL/Sources/Server/Database/database_gameobject_containers.h
@@ -5,6 +5,6 @@ class DatabaseGameObjectContainers
 public:
 	static void create(clan::SqliteConnection &db, int gameobject_container_id, const std::string &description);
 
-	static std::vector<int> get_active_gameobject_ids_in_container(clan::SqliteConnection &db, int gameobject_container_id);
+	static std::vector<int> get_load_by_default_gameobject_ids_in_container(clan::SqliteConnection &db, int gameobject_container_id);
 	static void move_gameobject_to_container(clan::SqliteConnection &db, int gameobject_id, int gameobject_container_id);
 };
