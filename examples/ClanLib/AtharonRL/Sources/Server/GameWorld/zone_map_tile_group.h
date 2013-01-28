eaee70b499fb96c7801ae891fb53ffca4013ffaa
ptrefall@gmail.com <ptrefall@gmail.com@1ed68ad0-91c7-136a-175d-75a241914234>
2013-01-28 13:53:18 +0000

    Some more work on setting up the new classes based on the recent discussion about the zones and maps on the server.

--- examples/ClanLib/AtharonRL/Sources/Server/GameWorld/zone_map_tile_group.h
+++ examples/ClanLib/AtharonRL/Sources/Server/GameWorld/zone_map_tile_group.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <memory>
+#include "Engine/Server/GameWorld/server_map_tile.h"
 
 class ServerMapTile; typedef std::shared_ptr<ServerMapTile> ServerMapTilePtr;
 
@@ -13,4 +14,13 @@ public:
 	void add(const ServerMapTilePtr &tile);
 	void add(const std::vector<ServerMapTilePtr> &tiles);
 	void remove(const ServerMapTilePtr &tile);
+
+private:
+	std::string name;
+	std::string prefab;
+
+	std::vector<ServerMapTilePtr> data; //Should this be replaced by a ZoneMapTileContainer?
+
+	clan::Point position;				//How is position defined? Center of tiles in the group? A corner?
+	clan::Point bounds;					//This is the bounds of the tile group, and will need to be dynamic based on add/remove of tiles...
 };
