d4b06977c26905c4a1bdf68b7cf2f6a32d4fded1
ptrefall@gmail.com <ptrefall@gmail.com@1ed68ad0-91c7-136a-175d-75a241914234>
2013-01-30 10:26:38 +0000

    Updated the HashedString class, for better linux compatibility. This required an update of the EventSystem too.

--- examples/ClanLib/AtharonRL/Include/Totem/HashedString.h
+++ examples/ClanLib/AtharonRL/Include/Totem/HashedString.h
@@ -2,6 +2,8 @@
  
 #include <functional>
 
+//This class is based on: http://www.altdevblogaday.com/2011/10/27/quasi-compile-time-string-hashing/
+
 #ifdef WIN32
 #define FORCE_INLINE __forceinline
 #else
@@ -45,23 +47,5 @@ private:
 	unsigned int hashId;
 	std::string str;
 };
-
-/*class HashFNV1a
-{
-public:
-	template<unsigned int N>
-	FORCE_INLINE static unsigned int hash(const char (&value)[N])
-	{
-		return (hash<N-1>((const char(&)[N-1])value)^ value[N-1]) * 16777619u;
-	}
-
-	template<N, 1>
-	FORCE_INLINE static unsigned int hash(const char (&value)[N])
-	{
-		return (2166136261u^ value[0]) * 16777619u;
-	}
-};
-*/
-typedef HashedString HashedString1;
 
 }
