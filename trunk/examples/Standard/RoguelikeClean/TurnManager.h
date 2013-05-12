f8290e816b21b3e44d2e370575efe54a32539450
PTrefall@gmail.com <PTrefall@gmail.com@1ed68ad0-91c7-136a-175d-75a241914234>
2013-05-12 09:27:34 +0000

    Code will currently not compile. I'm mid rewriting the turn-based system :)

--- trunk/examples/Standard/RoguelikeClean/TurnManager.h
+++ trunk/examples/Standard/RoguelikeClean/TurnManager.h
@@ -15,26 +15,50 @@ public:
 	~TurnManager();
 
 	void invoke();
-	void schedule(int time, const std::shared_ptr<Controller> &controller);
+	void schedule(int time, int actionType, const clan::Vec2i &direction, const std::shared_ptr<Controller> &controller);
 
 private:
 	unsigned long _turnCount;
 
-	struct ScheduleInfo
+	struct ActionInfo
 	{
 		bool Activated;
-		int TotalTime;
 		int CurrentTime;
+		int ActionType;
+		clan::Vec2i Direction;
+
+		ActionInfo(int time, int actionType, const clan::Vec2i &direction)
+			: Activated(false), CurrentTime(time), ActionType(actionType), Direction(direction)
+		{
+		}
+	};
+
+	struct ScheduleInfo
+	{
+		std::vector<ActionInfo> actions;
 		std::shared_ptr<Controller> controller;
-		ScheduleInfo(int time, const std::shared_ptr<Controller> &controller) : Activated(false), TotalTime(time), CurrentTime(time), controller(controller) {}
+		void add(const ActionInfo &action) 
+		{ 
+			actions.push_back(action); 
+		}
+		ScheduleInfo(const std::shared_ptr<Controller> &controller) : controller(controller) {}
 	};
 
 	std::vector<std::shared_ptr<ScheduleInfo>> _schedule;
 
+	void _runSchedule(int elapsedTime);
+	int _takeAction(const std::shared_ptr<ScheduleInfo> &info, int actionTime, int elapsedTime);
 	std::shared_ptr<ScheduleInfo> _find(const std::shared_ptr<Controller> &controller);
 
 	static bool ScheduleSorter(const std::shared_ptr<ScheduleInfo> &a, const std::shared_ptr<ScheduleInfo> &b)
 	{
-		return a->CurrentTime < b->CurrentTime;
+		if(a->actions.empty() && b->actions.empty())
+			return true;
+		else if(a->actions.empty())
+			return false;
+		else if(b->actions.empty())
+			return true;
+		else
+			return a->actions[0].CurrentTime < b->actions[0].CurrentTime;
 	}
 };
